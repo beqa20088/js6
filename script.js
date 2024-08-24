ee330063135166534737134b5b147f61d0d714b7
beqa20088 <138366831+beqa20088@users.noreply.github.com>
2024-08-24 05:12:12 +0400

    sadsda

---
+++ script.js
@@ -0,0 +1,9 @@
+let num = prompt('cahwere ricxvi');
+switch (num%2) {
+    case 'kenti':
+        console.log('ricxvi kentia');
+        break;
+    default:
+        console.log('ricxvi luwia');
+        break;
+}
